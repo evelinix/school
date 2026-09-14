d34912069ffd36cfe1b9eafbc23333a07932d658
Eve Lin (evelinix) <207277820+evelinix@users.noreply.github.com>
2026-09-14 08:54:04 -0400

    perbaiki tipe return DebugController

--- app/Http/Controllers/DebugController.php
+++ app/Http/Controllers/DebugController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
+
 class DebugController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         return view('information');
     }
